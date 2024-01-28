8a40bb2ef34dd32784dd5718f5ff5322d2d1f462
SantiMarich <santiagomarich@gmail.com>
2024-01-28 16:22:30 -0300

    ¨BackendOK¨

--- api/src/models/House.js
+++ api/src/models/House.js
@@ -29,7 +29,7 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      country: {
+      location: {
         type: DataTypes.STRING,
         allowNull: false,
       },
